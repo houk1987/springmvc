0b9d928daf16a27b6dd3f32aab2b589900884c9d
hou.kun <houk1987@gmail.com>
2020-04-16 10:46:33 +0800

    doLocationConfig

--- src/main/java/com/springmvc/servlet/HkDispatcherServlet.java
+++ src/main/java/com/springmvc/servlet/HkDispatcherServlet.java
@@ -1,4 +1,52 @@
 package com.springmvc.servlet;
 
-public class HkDispatcherServlet {
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class HkDispatcherServlet extends HttpServlet {
+
+    private Properties config;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        //加载配置
+        doLocationConfig(config.getInitParameter("contentConfigLocation"));
+
+        //解析配置，并扫描
+        doScanner();
+
+    }
+
+    private void doScanner() {
+        String scannerpackage = this.config.getProperty("scannerpackage");
+        System.out.println(scannerpackage);
+
+    }
+
+    private void doLocationConfig(String contentLocation) {
+        this.config = new Properties();
+        try(InputStream resourceAsStream = this.getClass().getResourceAsStream("/"+contentLocation)){
+            this.config.load(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
